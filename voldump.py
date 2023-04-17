5ae6d68bb560b070cbe85d3e526431c1824ccc24
flightcode <flightcode@pm.me>
2023-04-17 13:53:36 +0930

    Add image scan

--- voldump.py
+++ voldump.py
@@ -6,3 +6,19 @@
 ### Run VolDump.py in folder with memory images.
 ### Output will be named per image, with child folders.
 ## Last Updated 2023-04-17
+
+import os
+
+def main():
+    print("----------- VOLDUMP ------------")
+    print("--- Last Updated 2023-04-17 ----")
+    scanImages()
+
+def scanImages():
+    print("Scanning disk images...")
+    images = [each for each in os.listdir(".") if each.endswith('.dmp')]
+    print(f"Found {len(images)} image(s)")
+    for image in images:
+        print(f"- {image}")
+
+if __name__ == '__main__': main()
