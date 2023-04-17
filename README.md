cf7c1fe1bfefb33a3c21b10630e8c1aaf270b31e
flightcode <flightcode@pm.me>
2023-04-17 13:41:37 +0930

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# VolDump.py
+Volatility mass collection tool.
+
+## Usage
+1. Run `voldump.py` in folder with memory images.
+
+Output will be named per image, with child folders.
---
+++ voldump.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+## VolDump.py
+## Volatility mass collection tool
+## USAGE
+### Run VolDump.py in folder with memory images.
+### Output will be named per image, with child folders.
+## Last Updated 2023-04-17
