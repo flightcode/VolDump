65faeee7dc6d6b6c105bd1cfdc5c2f2685cde8ce
flightcode <flightcode@pm.me>
2023-04-17 14:10:35 +0930

    Convert to Bash

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# VolDump.py
+# VolDump
 Volatility mass collection tool.
 
 ## Usage
-1. Run `voldump.py` in folder with memory images.
+1. Run `voldump.sh` in folder with memory images.
 
 Output will be named per image, with child folders.
---
+++ voldump.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## VolDump
+## Volatility mass collection tool
+## USAGE
+### Run VolDump.sh in folder with memory images.
+### Output will be named per image, with child folders.
+## Last Updated 2023-04-17
+
+function main () {
+    echo "----------- VOLDUMP ------------"
+    echo "--- Last Updated 2023-04-17 ----"
+    scanImages
+}
+
+function scanImages () {
+    echo "Scanning disk images..."
+    shopt -s nullglob
+	images=(*.dmp)
+    echo "Found ${#images[@]} image(s)"
+}
+
+main
